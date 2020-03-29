873c5599e1441f8a91b05ded2ccb27ef2322d3e8
Anjia Wang <anjiawang@gmail.com>
2020-03-29 06:13:09 -0400

    Check whehter the output file exists before response

--- Flask_TSan.py
+++ Flask_TSan.py
@@ -59,7 +59,10 @@ def upload():
                 name = f.filename
         else:
             name = ""
-        print(name)
+
+        # 1. Remove all the files in the benchmark folder.
+        # 2. Copy the received input file to the benchmark folder.
+        # 3. Run the test and generate the log file.
         cmd_list = [
             "rm -rf /home/rds/dataracebench/micro-benchmarks/*",
             "cp " + os.path.join(app.config['UPLOAD_FOLDER'], name) + " /home/rds/dataracebench/micro-benchmarks/.",
@@ -71,7 +74,14 @@ def upload():
             output = result.stderr
         else:
             output = result.stdout
-        jsonResult = logjson.jsonify("/home/rds/dataracebench/results/log/" + name + ".tsan-clang.parser.log")
+
+        # The output log may not exist because the tool doesn't support the input.
+        # In this case, an empty JSON response will be returned.
+        logFile = "/home/rds/dataracebench/results/log/" + name + ".tsan-clang.parser.log"
+        if os.path.exists(logFile):
+            jsonResult = logjson.jsonify(logFile)
+        else:
+            jsonResult = flask.jsonify({})
         if request.args.get('type') == 'json':
             return flask.make_response(jsonResult, 200)
         else:
